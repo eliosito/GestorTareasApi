9603df027b1adcc90d9221c93a02414e81b2f2cf
eliosito <eliovillarroelsandoval@gmail.com>
2026-05-27 01:08:14 -0300

    formulario completo pero sin todas las funcionalidades, faltan estado ,fechas y prioridad. Luego lo demas se puede guardar y agregar al contenedor

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import Contenedor from './componentes/Contenedor'
 import Formulario from './componentes/Formulario'
 import Modal from './componentes/Modal'
 
-function App() {
 
-  const [abierto,setAbierto] = useState(false)
-
-  let tareas = [
+  let tareasDefault = [
     {    titulo: "Webo Wick",categoria: "Accion",estado:"Pendiente",FechaCreacion: "03-03-2026",FechaVencimiento: "05-05-2026",prioridad: "Alta",selecionado: false},
     {    titulo: "Mc Wick",categoria: "Accion",estado:"En proceso",FechaCreacion: "03-03-2026",FechaVencimiento: "05-05-2026",prioridad: "Media",selecionado: false},
     {    titulo: "Chino Wick",categoria: "Accion",estado:"Finalizado",FechaCreacion: "03-03-2026",FechaVencimiento: "05-05-2026",prioridad: "Baja",selecionado: false},
 
   ]
+
+function App() {
+
+  const [tareas, setTareas] = useState(tareasDefault)
+
+  const [abierto,setAbierto] = useState(false)
+
+  const guardar = (tarea) => {
+
+    console.log(tarea);
+    let nuevasTareas = [...tareas];
+    nuevasTareas.push(tarea)
+
+    setTareas(nuevasTareas)
+  }
+
+
 
   console.log({tareas})
 
@@ -29,7 +43,9 @@ function App() {
     cerrar={() => setAbierto(false)}
     >
 
-      <Formulario/>
+      <Formulario
+      guardar={(tarea) => guardar(tarea)}
+      />
 
 
     </Modal>
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,31 +1,80 @@
 import React from 'react'
 import useTarea from '../hooks/useTarea'
 
-export default function Formulario() {
+export default function Formulario({guardar}) {
 
     const [tarea, setDatoTarea] = useTarea();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        guardar({...tarea})
+    }
+
 
     return (
-    <div>
-        <input 
-        type="text" 
-        placeholder='Titulo'
-        
-        />
+        <form onSubmit={handleSubmit}>
+    <div className='formulario-contenedor'>
+        <div className='formulario-grupo'>
 
-        <textarea placeholder='Descripcion' rows="6" cols="30"> </textarea>
+            <input type="text" 
+            placeholder='Titulo'
+            onChange={(e) => setDatoTarea("titulo",e.target.value)}
+            value={tarea.titulo}
+            className='formulario-input'/>
+        </div>
+        <div className='formulario-grupo'>
+            <textarea 
+            placeholder='Descripcion'
+            rows="6"
+            onChange={(e) => setDatoTarea("descripcion",e.target.value)}
+            value={tarea.descripcion}
+            className='formulario-textarea'            
+            ></textarea>
+        </div>
 
-        <select
-        
-        >
-            <option value="" disabled>Seleccionar Categoria</option>
-            <option value="Hogar">Hogar</option>
-            <option value="Trabajo">Trabajo</option>
-        </select>
+        <div className='formulario-grupo'>
+            <select className='formulario-select'
+            onChange={(e) => setDatoTarea("categoria",e.target.value)}
+            value={tarea.categoria}
+            >
+                <option value="" disabled selected>Categoría</option>
+                <option value="Hogar">Hogar</option>
+                <option value="Trabajo">Trabajo</option>
+                <option value="Colegio">Colegio</option>
+                <option value="Personal">Personal</option>
+            </select>
+        </div>
 
-        
+        <div className='formulario-grupo'>
+            <div className='formulario-label'>Prioridad</div>
+            <div className='prioridad-opciones'>
+                <div className='radio-grupo'>
+                    <input type="radio" id="alta" name="prioridad" value="Alta" />
+                    <label htmlFor="alta">Alta</label>
+                </div>
+                <div className='radio-grupo'>
+                    <input type="radio" id="media" name="prioridad" value="Media" />
+                    <label htmlFor="media">Media</label>
+                </div>
+                <div className='radio-grupo'>
+                    <input type="radio" id="baja" name="prioridad" value="Baja" />
+                    <label htmlFor="baja">Baja</label>
+                </div>
+            </div>
+        </div>
+
+        <div className='formulario-grupo'>
+            <input 
+            type="date" 
+            className='formulario-input'
+            placeholder='DD / MM / AAAA'
+            />
+        </div>
+        <button ype="submit" className='formulario-boton'>Guardar</button>
+
 
     </div>
+    </form>
     )
 }
