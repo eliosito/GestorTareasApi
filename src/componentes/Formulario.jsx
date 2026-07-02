7336d79a41d47c7b44c02b81859deeedc9df8779
eliosito <eliovillarroelsandoval@gmail.com>
2026-07-01 21:11:06 -0300

    formulario rediseñado terminado, opciones sin uso eliminadas y tamaños corregidos

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -10,94 +10,92 @@ export default function Formulario() {
         e.preventDefault();
 
         const url = "https://api-tareas.ctpoba.edu.ar/api/tareas"
-        
+
         const config = {
-            headers: { Authorization: "48354750"}
+            headers: { Authorization: "48354750" }
         }
 
-        axios.post(url,tarea, config)
-        .then ((resp) => {
-            console.log(resp);
-            alert("Tarea guardada")
-        })
-        .catch((error) => {
-            console.error(error)
-            alert("Error al guardar")
-        })
+        axios.post(url, tarea, config)
+            .then((resp) => {
+                console.log(resp);
+                alert("Tarea guardada")
+            })
+            .catch((error) => {
+                console.error(error)
+                alert("Error al guardar")
+            })
 
-        
+
     }
 
 
     return (
-<form onSubmit={handleSubmit}>
-    <div className='formulario-contenedor'>
-        <div className='formulario-grupo'>
+        <form onSubmit={handleSubmit} className="formulario-raiz">
+            <div className='formulario-contenedor-imagen'>
 
-            <input type="text" 
-            placeholder='Nombre'
-            onChange={(e) => setDatoTarea("nombre",e.target.value)}
-            value={tarea.nombre}
-            className='formulario-input'/>
-        </div>
-        <div className='formulario-grupo'>
-            <textarea 
-            placeholder='Descripcion'
-            rows="6"
-            onChange={(e) => setDatoTarea("descripcion",e.target.value)}
-            value={tarea.descripcion}
-            className='formulario-textarea'            
-            ></textarea>
-        </div>
+                {/* COLUMNA IZQUIERDA */}
+                <div className='columna-izquierda'>
+                    <div className='formulario-grupo'>
+                        <label className='formulario-label'>Titulo</label>
+                        <input type="text"
+                            onChange={(e) => setDatoTarea("nombre", e.target.value)}
+                            value={tarea.nombre}
+                            className='formulario-input'
+                        />
+                    </div>
 
-        <div className='formulario-grupo'>
-            <select className='formulario-select'
-            onChange={(e) => setDatoTarea("categoria",e.target.value)}
-            value={tarea.categoria}
-            >
-                <option value="" disabled selected>Categoría</option>
-                <option value="Hogar">Hogar</option>
-                <option value="Trabajo">Trabajo</option>
-                <option value="Colegio">Colegio</option>
-                <option value="Personal">Personal</option>
-            </select>
-        </div>
+                    <div className='formulario-grupo'>
+                        <label className='formulario-label'>Categoria</label>
+                        <select className='formulario-select'
+                            onChange={(e) => setDatoTarea("categoria", e.target.value)}
+                            value={tarea.categoria}
+                        >
+                            <option value="" disabled selected></option>
+                            <option value="Hogar">Hogar</option>
+                            <option value="Trabajo">Trabajo</option>
+                            <option value="Colegio">Colegio</option>
+                            <option value="Personal">Personal</option>
+                        </select>
+                    </div>
 
-        <div className='formulario-grupo'>
-            <div className='formulario-label'>Prioridad</div>
-
-            <div className='prioridad-opciones'>
-
-
-                <div className='radio-grupo'>
-
-                    <input type="radio" id="alta" name="prioridad"  value="1"  onChange={(e) => setDatoTarea("prioridad",e.target.value)} checked={tarea.prioridad === "1"} />
-                    <label htmlFor="alta">Alta</label>
-
+                    <div className='formulario-grupo'>
+                        <label className='formulario-label'>Prioridad</label>
+                        <div className='prioridad-opciones-imagen'>
+                            <div className='radio-grupo-imagen'>
+                                <input type="radio" id="alta" name="prioridad" value="1" onChange={(e) => setDatoTarea("prioridad", e.target.value)} checked={tarea.prioridad === "1"} />
+                                <label htmlFor="alta" className="text-alta">Alta</label>
+                            </div>
+                            <div className='radio-grupo-imagen'>
+                                <input type="radio" id="media" name="prioridad" value="2" onChange={(e) => setDatoTarea("prioridad", e.target.value)} checked={tarea.prioridad === "2"} />
+                                <label htmlFor="media" className="text-media">Media</label>
+                            </div>
+                            <div className='radio-grupo-imagen'>
+                                <input type="radio" id="baja" name="prioridad" value="3" onChange={(e) => setDatoTarea("prioridad", e.target.value)} checked={tarea.prioridad === "3"} />
+                                <label htmlFor="baja" className="text-baja">Baja</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className='radio-grupo'>
 
-                    <input type="radio" id="media" name="prioridad" value="2" onChange={(e) => setDatoTarea("prioridad",e.target.value)} checked={tarea.prioridad === "2"}/>
-                    <label htmlFor="media">Media</label>
+                <div className='columna-derecha'>
+r
 
+                    <div className='formulario-grupo grupo-textarea-full'>
+                        <label className='formulario-label'>Descripcion de tarea</label>
+                        <textarea
+                            onChange={(e) => setDatoTarea("descripcion", e.target.value)}
+                            value={tarea.descripcions}
+                            className='formulario-textarea-imagen'
+                        ></textarea>
+                    </div>
+
+                    <div className='contenedor-boton-guardar'>
+                        <button type="submit" className='formulario-boton-imagen'>Guardar</button>
+                    </div>
                 </div>
-                <div className='radio-grupo'>
 
-                    <input type="radio" id="baja" name="prioridad" value="3" onChange={(e) => setDatoTarea("prioridad",e.target.value)} checked={tarea.prioridad === "3"} />
-                    <label htmlFor="baja">Baja</label>
-
-                </div>
-                
             </div>
-
-        </div>
-
-        <button type="submit" className='formulario-boton'>Guardar</button>
-
-
-    </div>
-    </form>
-        
+        </form>
     )
 }
 
